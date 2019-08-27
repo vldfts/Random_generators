f1fad01f357952b305ea5aa6f2622382e1469bf2
vldf <vldfts@outlook.com>
2019-08-28 02:38:02 +0300

    Добавьте файлы проекта.

---
+++ As_Cript_WPF/MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace As_Cript_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddFilesToCombox();
+        }
+
+        private void AddFilesToCombox()
+        {
+            string path = Directory.GetCurrentDirectory() + "\\data\\";
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                foreach (string item in files)
+                {
+                    string item2 = item.Remove(0, Directory.GetCurrentDirectory().Length);
+                    ComboBoxItem it = new ComboBoxItem();
+                    it.Content = item2;
+                    combox_files.Items.Add(it);
+                }
+            }
+        }
+
+
+        private void gen_click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(kilk.Text);
+            result.BorderBrush = new SolidColorBrush(Colors.Gray);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            switch (generator.SelectedIndex)
+            {
+                case 0:
+                    textBuffer.Text += Model.Vstr(n);
+                    break;
+                case 1:
+                    textBuffer.Text += Model.Lehmer(true, n);
+                    break;
+                case 2:
+                    textBuffer.Text += Model.Lehmer(false, n);
+                    break;
+                case 3:
+                    textBuffer.Text += Model.L20(n);
+                    break;
+                case 4:
+                    textBuffer.Text += Model.L89(n);
+                    break;
+                case 5:
+                    textBuffer.Text += Model.Geffe(n);
+                    break;
+                case 6:
+                    textBuffer.Text += Model.Biblio();
+                    break;
+                case 7:
+                    textBuffer.Text += Model.Wolfram(n);
+                    break;
+                case 8:
+                    textBuffer.Text += Model.BM(true, n);
+                    break;
+                case 9:
+                    textBuffer.Text += Model.BM(false, n);
+                    break;
+                case 10:
+                    textBuffer.Text += Model.BBS(true, n);
+                    break;
+                case 11:
+                    textBuffer.Text += Model.BBS(false, n);
+                    break;
+                default: break;
+            }
+            sw.Stop();
+            timer.Content = (double)sw.ElapsedMilliseconds/1000 +" сек";
+        }
+
+        private void start_button_Click(object sender, RoutedEventArgs e)
+        {
+            double[] sho_tam = new double[3];
+            string bytePoslidovn = "";
+            double kvatl_norm_rozpodil = 0;
+            try
+            {
+                if (textBuffer.Text.Substring(0, 5).Contains(' '))
+                {
+                    bytePoslidovn = textBuffer.Text;
+                }
+                else
+                {
+
+                    bytePoslidovn = Model.Peretvor_Bit_To_Byte(new StringBuilder(textBuffer.Text));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                textBuffer.Text = ex.Message;
+            }
+            switch (kvantil.SelectedIndex)
+            {
+                case 0:
+                    kvatl_norm_rozpodil = 2.326;
+                    break;
+                case 1:
+                    kvatl_norm_rozpodil = 1.6449;
+                    break;
+                case 2:
+                    kvatl_norm_rozpodil = 1.2816;
+                    break;
+                default:
+                    break;
+            }
+            switch (test.SelectedIndex)
+            {
+                case 0:
+                    sho_tam = Model.Rivnomirnist(new StringBuilder(bytePoslidovn), kvatl_norm_rozpodil);
+                    break;
+                case 1:
+                    sho_tam = Model.Nezalezhnist(new StringBuilder(bytePoslidovn), kvatl_norm_rozpodil);
+                    break;
+                case 2:
+                    sho_tam = Model.Odnoridnist(new StringBuilder(bytePoslidovn), kvatl_norm_rozpodil);
+                    break;
+                default:
+                    break;
+            }
+            result.Content = "Пр " +Math.Round(sho_tam[0],3)+" -- "+ Math.Round(sho_tam[2], 3)+ " Теор";
+            if (sho_tam[1] == 1)
+            {
+                result.BorderBrush = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                result.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+        }
+
+        private void del_button_Click(object sender, RoutedEventArgs e)
+        {
+            textBuffer.Text = "";
+            timer.Content = "";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                label_open_file.Foreground = new SolidColorBrush(Colors.Black);
+                ComboBoxItem o = (ComboBoxItem)combox_files.SelectedItem;
+                textBuffer.Text += File.ReadAllText(Directory.GetCurrentDirectory() + o.Content.ToString());
+            }
+            catch
+            {
+                label_open_file.Foreground = new SolidColorBrush(Colors.Red);
+                textBuffer.Text = "Спочатку виберіть файл!!!";
+            }
+
+        }
+
+        private void downl_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem o = (ComboBoxItem)combox_files.SelectedItem;
+            using (StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory() + o.Content.ToString(), false))
+            {
+                sw.Write(textBuffer.Text);
+            }
+        }
+    }
+}
---
+++ As_Cript_WPF/Model.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace As_Cript_WPF
+{
+    static class Model
+    {
+        static Random rand = new Random();
+        public static StringBuilder Vstr(int n)
+        {
+            StringBuilder vidp = new StringBuilder();
+            for (int i = 0; i < n / 32; i++)
+            {
+                vidp.Append(Convert.ToString(rand.Next(1, int.MaxValue), 2).PadLeft(32, '0'));
+            }
+            return vidp;
+
+        }
+        public static StringBuilder Lehmer(bool index, int n)
+        {
+            double m = Math.Pow(2, 32);
+            double a = Math.Pow(2, 16) + 1;
+            int c = 119;
+            double x0 = rand.Next(1, int.MaxValue);
+            StringBuilder vidp = new StringBuilder();
+            if (!index)//High
+            {
+                for (int i = 0; i < n / 8; i++)
+                {
+                    x0 = (a * x0 + c) % m;
+                    vidp.Append(Convert.ToString(Convert.ToUInt32(x0), 2).PadLeft(32, '0').Substring(0, 8));
+                }
+            }
+            else//Low
+            {
+                for (int i = 0; i < n / 8; i++)
+                {
+                    x0 = (a * x0 + c) % m;
+                    vidp.Append(Convert.ToString(Convert.ToUInt32(x0), 2).PadLeft(32, '0').Substring(24));
+                }
+            }
+            return vidp;
+        }
+        public static StringBuilder L20(int n)
+        {
+            string x = Convert.ToString(rand.Next(1, 2 << 19), 2).PadLeft(20, '0');
+            StringBuilder vidp = new StringBuilder(x);
+            for (int i = 0; i < n - 20; i++)
+            {
+                vidp.Append(x[0] ^ x[11] ^ x[15] ^ x[17]);
+                x = (x + (x[0] ^ x[11] ^ x[15] ^ x[17])).Substring(1, 20);
+            }
+            return vidp;
+        }
+        public static StringBuilder L89(int n)
+        {
+            byte[] mas = new byte[12];
+            rand.NextBytes(mas);
+            string x = "";
+            for (int i = 0; i < 12; i++)
+            {
+                x += Convert.ToString(mas[i], 2);
+            }
+            x = x.PadLeft(89, '0').Substring(0, 89);
+            StringBuilder vidp = new StringBuilder(x);
+            for (int i = 0; i < n - 89; i++)
+            {
+                vidp.Append(x[0] ^ x[51]);
+                x = (x + (x[0] ^ x[51])).Substring(1, 89);
+            }
+            return vidp;
+        }
+        public static StringBuilder Geffe(int n)
+        {
+            string L11 = Convert.ToString(rand.Next(1, 2 << 10), 2).PadLeft(11, '0').Substring(0, 11);
+            string L9 = Convert.ToString(rand.Next(1, 2 << 8), 2).PadLeft(9, '0').Substring(0, 9);
+            string L10 = Convert.ToString(rand.Next(1, 2 << 9), 2).PadLeft(10, '0').Substring(0, 10);
+            StringBuilder z = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                if (L10[0] == '1')
+                {
+                    z.Append(L11[0]);
+                }
+                else { z.Append(L9[0]); }
+                L11 = (L11 + (L11[0] ^ L11[2])).Substring(1, 11);
+                L9 = (L9 + (L9[0] ^ L9[1] ^ L9[3] ^ L9[4])).Substring(1, 9);
+                L10 = (L10 + (L10[0] ^ L10[3])).Substring(1, 10);
+            }
+            return z;
+        }
+        public static StringBuilder Wolfram(int n)
+        {
+            StringBuilder x = new StringBuilder();
+            int r = rand.Next(1, int.MaxValue);
+            for (int i = 0; i < n; i++)
+            {
+                x.Append(Math.Abs(r % 2));
+                r = ((r << 1)) ^ (r | ((r >> 1)));
+            }
+            return x;
+        }
+        public static StringBuilder Biblio()
+        {
+            string path = "//texts//text.txt";
+            StringBuilder data = new StringBuilder();
+            byte[] bytes;
+            using (StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + path))
+            {
+                bytes = Encoding.UTF8.GetBytes(sr.ReadToEnd());
+            }
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                data.Append(bytes[i] + " ");
+            }
+            return data;
+        }
+        public static StringBuilder BM(bool index, int n)
+        {
+            string p = "CEA42B987C44FA642D80AD9F51F10457690DEF10C83D0BC1BCEE12FC3B6093E3";
+            string a = "5B88C41246790891C095E2878880342E88C79974303BD0400B090FE38A688356";
+            BigInteger numb = BigInteger.Abs(BigInteger.Parse(p, System.Globalization.NumberStyles.HexNumber)) - 1;
+            BigInteger numb2 = BigInteger.Parse(a, System.Globalization.NumberStyles.HexNumber);
+            BigInteger rivn = BigInteger.Divide(numb, 2);
+            byte[] bytes = numb.ToByteArray();
+            BigInteger T;
+            while (true)
+            {
+                rand.NextBytes(bytes);
+                T = BigInteger.Abs(new BigInteger(bytes));
+                if (BigInteger.Compare(T, numb) != -1)
+                {
+                    break;
+                }
+            }
+            StringBuilder vidpovid = new StringBuilder();
+            if (index)
+            {
+                if (BigInteger.Compare(T, rivn) < 0)
+                {
+                    vidpovid.Append("1");
+                }
+                else
+                {
+                    vidpovid.Append("0");
+                }
+                for (int i = 0; i < n - 1; i++)
+                {
+                    T = BigInteger.ModPow(numb2, T, numb);
+                    if (BigInteger.Compare(T, rivn) < 0)
+                    {
+                        vidpovid.Append("1");
+                    }
+                    else
+                    {
+                        vidpovid.Append("0");
+                    }
+
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n / 8; i++)
+                {
+                    T = BigInteger.ModPow(numb2, T, numb);
+                    for (int k = 0; k < 256; k++)
+                    {
+                        if (BigInteger.Compare(BigInteger.Divide(BigInteger.Multiply(k, numb), 256), T) < 0 && BigInteger.Compare(BigInteger.Divide(BigInteger.Multiply(k + 1, numb), 256), T) >= 0)
+                        {
+                            vidpovid.Append(k + " ");
+                            break;
+                        }
+                    }
+                }
+            }
+            return vidpovid;
+
+
+        }
+        public static StringBuilder BBS(bool index, int n)
+        {
+            string p = "D5BBB96D30086EC484EBA3D7F9CAEB07";
+            string q = "425D2B9BFDB25B9CF6C416CC6E37B59C1F";
+            BigInteger numb = BigInteger.Parse(p, System.Globalization.NumberStyles.HexNumber);
+            BigInteger numb2 = BigInteger.Parse(q, System.Globalization.NumberStyles.HexNumber);
+            BigInteger numb3 = BigInteger.Multiply(numb, numb2);
+            byte[] bytes = numb3.ToByteArray();
+            BigInteger r = rand.Next(2, int.MaxValue);
+            StringBuilder vidpovid = new StringBuilder();
+            if (index)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    r = BigInteger.ModPow(r, 2, numb3);
+                    vidpovid.Append(r % 2);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n / 8; i++)
+                {
+                    r = BigInteger.ModPow(r, 2, numb3);
+                    vidpovid.Append(r % 256 + " ");
+                }
+            }
+            return vidpovid;
+        }
+        public static string Peretvor_Bit_To_Byte(StringBuilder bitPoslidovn)
+        {
+            string bitPoslidovn2 = bitPoslidovn.ToString();
+            string new_mas = "";
+            for (int i = 0; i < bitPoslidovn2.Length - 7; i += 8)
+            {
+                new_mas += Convert.ToByte(bitPoslidovn2.Substring(i, 8), 2) + " ";
+            }
+            return new_mas;
+        }
+        public static int[] Statystyka_Vhodjen_Byte(StringBuilder bytePoslidovn)
+        {
+            string bytePoslidovn2 = bytePoslidovn.ToString();
+            string[] a = bytePoslidovn2.Split(' ');
+            int[] list_kilk_vhodj = new int[256];
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                list_kilk_vhodj[int.Parse(a[i])] += 1;
+            }
+            return list_kilk_vhodj;
+        }
+        public static double[] Rivnomirnist(StringBuilder bytePoslidovn, double kvatl_norm_rozpodil)
+        {
+            int[] list_kilk_vhodj = Statystyka_Vhodjen_Byte(bytePoslidovn);
+            double[] mas = new double[3];
+            double Xi2 = 0;
+            string bytePoslidovn2 = bytePoslidovn.ToString();
+            int vh_mas = 0;
+            for (int i = 0; i < list_kilk_vhodj.Length; i++)
+            {
+                vh_mas += list_kilk_vhodj[i];
+            }
+            double nj = (double)vh_mas / 256;
+            for (int i = 0; i < 256; i++)
+            {
+                if (list_kilk_vhodj[i] != 0)
+                {
+                    Xi2 += Math.Pow(list_kilk_vhodj[i] - nj, 2) / Convert.ToDouble(nj);
+                }
+            }
+            mas[0] = Xi2;
+            mas[2] = Math.Sqrt(510) * kvatl_norm_rozpodil + 255;
+            if (Xi2 <= mas[2]) { mas[1] = 1; }
+            else { mas[1] = 0; }
+            return mas;
+        }
+        public static double[] Nezalezhnist(StringBuilder bytePoslidovn, double kvatl_norm_rozpodil)
+        {
+            double[] mas = new double[3];
+            double Xi2 = 0;
+            string bytePoslidovn2 = bytePoslidovn.ToString();
+            string[] a = bytePoslidovn2.Split(' ');
+            int length = a.Length - 1;
+            int[,] vij = new int[256, 256];
+            int[] vi = new int[256];
+            int[] aj = new int[256];
+            for (int i = 0; i < a.Length - 2; i += 2)
+            {
+                vij[int.Parse(a[i]), int.Parse(a[i + 1])]++;
+                vi[int.Parse(a[i])]++;
+                aj[int.Parse(a[i + 1])]++;
+            }
+            for (int ij = 0; ij < 256; ij++)
+            {
+                for (int jk = 0; jk < 256; jk++)
+                {
+                    if (vij[ij, jk] != 0)
+                    {
+                        Xi2 += Math.Pow(vij[ij, jk], 2) / Convert.ToDouble(vi[ij] * aj[jk]);
+
+                    }
+                }
+            }
+            Xi2 = (Xi2 - 1) * length / 2;
+            mas[0] = Xi2;
+            mas[2] = Math.Sqrt(2) * 255 * kvatl_norm_rozpodil + 255 * 255;
+            if (Xi2 <= mas[2]) { mas[1] = 1; }
+            else { mas[1] = 0; }
+            return mas;
+        }
+        public static double[] Odnoridnist(StringBuilder bytePoslidovn, double kvatl_norm_rozpodil)
+        {
+            int r = 10;
+            double[] mas = new double[3];
+            string bytePoslidovn2 = bytePoslidovn.ToString();
+            string[] vh_mas = bytePoslidovn2.Split(' ');
+            int[,] odn_mas = new int[r, 256];
+            int length = vh_mas.Length - 1;
+            int[] vi = new int[256];
+            for (int k = 0; k < r; k++)
+            {
+                for (int j = 0; j < length / r; j++)
+                {
+                    odn_mas[k, int.Parse(vh_mas[k * (length / r) + j])]++;
+                    vi[int.Parse(vh_mas[k * (length / r) + j])]++;
+                }
+
+            }
+            double Xi2 = 0;
+            double aj = length / r;
+            for (int ij = 0; ij < r; ij++)
+            {
+                for (int jk = 0; jk < 256; jk++)
+                {
+                    if (odn_mas[ij, jk] != 0)
+                    {
+                        Xi2 += Math.Pow(odn_mas[ij, jk], 2) / Convert.ToDouble(vi[jk] * aj);
+                    }
+                }
+            }
+            Xi2 = (Xi2 - 1) * length;
+            mas[0] = Xi2;
+            mas[2] = Math.Sqrt(2 * 255 * (r - 1)) * kvatl_norm_rozpodil + 255 * (r - 1);
+            if (Xi2 <= mas[2]) { mas[1] = 1; }
+            else { mas[1] = 0; }
+            return mas;
+        }
+
+    }
+}
+
